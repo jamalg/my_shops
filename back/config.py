c7cb30f569232a2744fa0f447e478db0cd4df417
Jamal Gourinda <jamal.gourinda.pro@gmail.com>
2019-06-19 11:44:58 +0100

    feat: add config file

---
+++ back/config.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+from back.utils.config_class import BaseConfig, EnvironmentVariable, BoolEnvironmentVariable, IntEnvironmentVariable
+
+
+class Config(BaseConfig):
+    # App settings
+    # --> General
+    APP_NAME = "my-shops"
+    ENVIRONMENT = EnvironmentVariable()
+    TESTING = BoolEnvironmentVariable(default=False)
+    # --> Security
+    SECRET_KEY = EnvironmentVariable()
+    BCRYPT_LOG_ROUNDS = IntEnvironmentVariable(default=4)
+    BCRYPT_HANDLE_LONG_PASSWORDS = True
+
+    # External Services
+    # --> DB
+    POSTGRES_USER = EnvironmentVariable()
+    POSTGRES_PASSWORD = EnvironmentVariable()
+    POSTGRES_DB = EnvironmentVariable()
+    POSTGRES_DB_TEST = EnvironmentVariable()
+    POSTGRES_HOST = EnvironmentVariable()
+
+    def __init__(self) -> None:
+        if self.TESTING:
+            self.POSTGRES_DB = self.POSTGRES_DB_TEST
+            self.LOGIN_DISABLED = True
+
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> str:
+        return "postgresql://{}:{}@{}:5432/{}".format(
+            self.POSTGRES_USER, self.POSTGRES_PASSWORD, self.POSTGRES_HOST, self.POSTGRES_DB
+        )
+
+
+config = Config()
+
+
+def init_app(app: Flask) -> None:
+    app.config.from_object(config)
