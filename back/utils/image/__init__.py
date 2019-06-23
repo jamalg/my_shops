4988dd732fd402822e765be8066d672606b730ea
Jamal Gourinda <jamal.gourinda.pro@gmail.com>
2019-06-23 10:27:12 +0100

    feat: add serialization/deserialization for Image util

--- back/utils/image/__init__.py
+++ back/utils/image/__init__.py
@@ -1,7 +1,22 @@
 from typing import NamedTuple
 
+BINARY_SEPARATOR = b"|"
+
 
 class Image(NamedTuple):
     binary: bytes
     filename: str
     mimetype: str
+
+    @staticmethod
+    def serialize(image):
+        # FIXME Highly experimental
+        return BINARY_SEPARATOR.join([image.binary, image.filename.encode(), image.mimetype.encode()])
+
+    @classmethod
+    def deserialize(cls, serialized):
+        # FIXME Extremely hacky
+        *binary, byte_filename, byte_mimetype = serialized.split(BINARY_SEPARATOR)
+        binary = BINARY_SEPARATOR.join(binary)
+        filename, mimetype = byte_filename.decode(), byte_mimetype.decode()
+        return cls(binary=binary, filename=filename, mimetype=mimetype)
