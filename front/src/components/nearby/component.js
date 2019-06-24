2a19dfb88f1bf2667c7d1b3486d66de565e4d40b
Jamal Gourinda <jamal.gourinda.pro@gmail.com>
2019-06-24 16:25:59 +0100

    refactor: use new PlaceCard component in Nearby

--- front/src/components/nearby/component.js
+++ front/src/components/nearby/component.js
@@ -1,17 +1,7 @@
 import React from 'react'
 
+import PlaceCard from '../place'
 import './style.css'
-
-const PlaceCard = ({id, name, photoUrl, addLike, addDisLike}) => (
-    <div className="place-card">
-        <div className="place-card-title">{name}</div>
-        <img className="place-card-image" src={photoUrl} alt={`Illustration for ${name}`} />
-        <div className="place-social mt-2">
-            <button onClick={() => addDisLike(id)} className="btn btn-danger col-6 mr-1" >Dislike</button>
-            <button onClick={() => addLike(id)} className="btn btn-success col-6 mrl-1" >Like</button>
-        </div>
-    </div>
-)
 
 export const Nearby = ({ places, addLike, addDisLike }) => (
     <div className="nearby-wrapper">
